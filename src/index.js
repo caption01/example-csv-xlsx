ed2ca9e8e359e58224c619cdfb6f6c064841e3b9
APPMAN M-095 <nutchapon.han@gmail.com>
2021-05-03 22:23:55 +0700

    first edit data and write csv

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const XLSX = require("xlsx");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const { map } = require("lodash");
+
+const readdir = util.promisify(fs.readdir);
+
+const readFiles = async () => {
+  let filesList;
+  const sourcePath = path.join(__dirname, "../source");
+
+  try {
+    filesList = await readdir(sourcePath);
+    const workbook = XLSX.readFile(`${sourcePath}/${filesList[0]}`);
+
+    const sheetName = workbook.SheetNames[0];
+    const workSheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(workSheet);
+
+    const mapData = map(jsonData, (d) => ({
+      ...d,
+      gender: `${d.gender}/X`,
+    }));
+
+    // add edit data
+    XLSX.utils.sheet_add_json(workSheet, mapData);
+
+    XLSX.writeFile(workbook, "out3.csv");
+  } catch (err) {
+    console.log({ err });
+  }
+};
+
+readFiles();
