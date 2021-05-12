aa07ff8e455d4d2c0f7d3c98e83b4a696dced0d7
Ajarnshilin <ajarnnotes@gmail.com>
2021-05-12 15:56:32 +0700

    Add map columns

--- src/index.js
+++ src/index.js
@@ -15,14 +15,43 @@ const readFiles = async () => {
     filesList = await readdir(sourcePath);
     const workbook = XLSX.readFile(`${sourcePath}/${filesList[0]}`);
 
+    // console.log(workbook, filesList)
+
     const sheetName = workbook.SheetNames[0];
     const workSheet = workbook.Sheets[sheetName];
     const jsonData = XLSX.utils.sheet_to_json(workSheet);
 
-    const mapData = map(jsonData, (d) => ({
-      ...d,
-      gender: `${d.gender}/X`,
-    }));
+    // console.log(jsonData)
+
+
+    const mapData = map(jsonData, (d) => {
+      // console.log(d)
+      return {
+        Failed_count: ``,
+        id: `${d.id}`,
+        gender: `${d.gender}`,
+        age_inyear: `${d.age_inyear}`,
+        age_inmonth: `${d.age_inmonth}`,
+        hirate: `${d.hirate}`,
+        occuclass: `${d.occuclass}`,
+        paOccuclass: `${d.paOccuclass}`,
+        BasicPlan: `${d.BasicPlan}`,
+        Basic_SumAssured: `${d.Basic_SumAssured}`,
+        Basic_Premium: ``,
+        Payer_Gender: ``,
+        Payer_age_inyear:`${d.age_inyear}`,
+        Payer_age_inmonth:`${d.age_inmonth}`,
+        Payer_Type:``,
+        PaymentMode: ``,
+        Channel: ``,
+        productGroup: ``,
+        ridersSumassured: `${d['riders:Sumassured']}`,
+        code_result: ``,
+        code_actual: ``,
+        code_expected: `${d.code_expected}`
+      }
+
+    });
 
     // add edit data
     XLSX.utils.sheet_add_json(workSheet, mapData);
